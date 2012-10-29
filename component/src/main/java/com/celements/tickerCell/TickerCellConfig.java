269f25a6f1877e36fd0ce9010d7e2232f0425b4c
Fabian Pichler <fabian.pichler@synventis.com>
2012-10-29 21:31:53 +0100

    fix renderTemplates not resolving for templates on disk

--- component/src/main/java/com/celements/tickerCell/TickerCellConfig.java
+++ component/src/main/java/com/celements/tickerCell/TickerCellConfig.java
@@ -3,12 +3,24 @@ package com.celements.tickerCell;
 import java.util.Arrays;
 import java.util.List;
 
+import org.xwiki.context.Execution;
+
 import com.celements.common.classes.IClassCollectionRole;
 import com.celements.pagetype.IPageTypeConfig;
+import com.celements.pagetype.PageType;
 import com.celements.pagetype.PageTypeReference;
+import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.web.Utils;
 
 public class TickerCellConfig implements IPageTypeConfig {
+
+  private XWikiContext getContext() {
+    return (XWikiContext) getExecution().getContext().getProperty("xwikicontext");
+  }
+
+  private Execution getExecution() {
+    return Utils.getComponent(Execution.class);
+  }
 
   public boolean displayInFrameLayout() {
     return true;
@@ -33,11 +45,16 @@ public class TickerCellConfig implements IPageTypeConfig {
 
   public String getRenderTemplateForRenderMode(String renderMode) {
     if ("view".equals(renderMode)) {
-      return "Templates.TickerCellView";
+      return resolveTemplate("Templates.TickerCellView");
     } else if ("edit".equals(renderMode)) {
-      return "Templates.TickerCellEdit";
+      return resolveTemplate("Templates.TickerCellEdit");
     }
     return "";
+  }
+
+  private String resolveTemplate(String specView) {
+    return new PageType("PageTypes.TickerCell").resolveTemplatePath(specView,
+        getContext());
   }
 
   public boolean hasPageTitle() {
@@ -49,4 +66,5 @@ public class TickerCellConfig implements IPageTypeConfig {
         "com.celements.tickerCell.classcollection")).isActivated();
   }
 
+  
 }
