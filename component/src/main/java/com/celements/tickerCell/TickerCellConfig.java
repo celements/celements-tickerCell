f20a3c114ea268f91cec51bbc4baf838d59f2b09
Marc Sladek <marc.sladek@synventis.com>
2018-07-06 09:46:54 +0200

    Release 3.0 (#1)
    * update to 3.0
    
    * release dependencies
    
    * [maven-release-plugin] prepare release celements-tickerCell-3.0
    
    * [maven-release-plugin] prepare for next development iteration
    
    * release tickerCell
    
    * [maven-release-plugin] prepare release tickerCell-web-3.0
    
    * [maven-release-plugin] prepare for next development iteration

--- component/src/main/java/com/celements/tickerCell/TickerCellConfig.java
+++ component/src/main/java/com/celements/tickerCell/TickerCellConfig.java
@@ -3,12 +3,17 @@ package com.celements.tickerCell;
 import java.util.Arrays;
 import java.util.List;
 
-import org.xwiki.context.Execution;
+import javax.validation.constraints.NotNull;
 
+import org.xwiki.context.Execution;
+import org.xwiki.model.reference.DocumentReference;
+
+import com.celements.cells.attribute.AttributeBuilder;
 import com.celements.common.classes.IClassCollectionRole;
 import com.celements.pagetype.IPageTypeConfig;
 import com.celements.pagetype.PageType;
 import com.celements.pagetype.PageTypeReference;
+import com.google.common.base.Optional;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.web.Utils;
 
@@ -22,10 +27,12 @@ public class TickerCellConfig implements IPageTypeConfig {
     return Utils.getComponent(Execution.class);
   }
 
+  @Override
   public boolean displayInFrameLayout() {
     return true;
   }
 
+  @Override
   public List<String> getCategories() {
     return Arrays.asList("pageType", "celltype");
   }
@@ -35,14 +42,17 @@ public class TickerCellConfig implements IPageTypeConfig {
         getCategories());
   }
 
+  @Override
   public String getName() {
     return "TickerCell";
   }
 
+  @Override
   public String getPrettyName() {
     return "Ticker Cell";
   }
 
+  @Override
   public String getRenderTemplateForRenderMode(String renderMode) {
     if ("view".equals(renderMode)) {
       return resolveTemplate("Templates.TickerCellView");
@@ -53,18 +63,39 @@ public class TickerCellConfig implements IPageTypeConfig {
   }
 
   private String resolveTemplate(String specView) {
-    return new PageType("PageTypes.TickerCell").resolveTemplatePath(specView,
-        getContext());
+    return new PageType("PageTypes.TickerCell").resolveTemplatePath(specView, getContext());
   }
 
+  @Override
   public boolean hasPageTitle() {
     return false;
   }
 
+  @Override
   public boolean isVisible() {
-    return ((TickerCellClassCollection)Utils.getComponent(IClassCollectionRole.class,
+    return ((TickerCellClassCollection) Utils.getComponent(IClassCollectionRole.class,
         "com.celements.tickerCell.classcollection")).isActivated();
   }
 
-  
+  @Override
+  public boolean isUnconnectedParent() {
+    return false;
+  }
+
+  @Override
+  public boolean useInlineEditorMode() {
+    return false;
+  }
+
+  @Override
+  @NotNull
+  public Optional<String> defaultTagName() {
+    return Optional.absent();
+  }
+
+  @Override
+  public void collectAttributes(@NotNull AttributeBuilder attrBuilder,
+      @NotNull DocumentReference cellDocRef) {
+  }
+
 }
